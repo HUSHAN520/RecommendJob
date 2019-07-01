59f3872785b2d7db66cdb2cad0603d6368637b73
HUSHAN520 <32026979+HUSHAN520@users.noreply.github.com>
2019-07-01 00:54:03 -0700

    Update Application.java

--- springboot/src/main/java/com/example/project/Application.java
+++ springboot/src/main/java/com/example/project/Application.java
@@ -1,46 +1,14 @@
 package com.example.project;
 
-import com.example.project.services.DoWork;
-import org.apache.calcite.avatica.remote.RemoteService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.Banner;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import ...
 
 @SpringBootApplication
-/*public class Application {
+public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-    }
-
-}*/
-public class Application implements CommandLineRunner {
-    @Autowired
-    private DoWork doWork;
-
-
-    public static void main(String[] args) {
-        SpringApplication app = new SpringApplication(Application.class);
-        app.setBannerMode(Banner.Mode.OFF);
-        app.run(args);
-    }
-
-    @Override
-    public void run(String... args) throws Exception {
-        doWork.work();
     }
 
 }
 
-/*
-public class Application {
 
-
-    public static void main(String[] args) {
-
-        SpringApplication.run(Application.class, args);
-
-    }
-}*/
